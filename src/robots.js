c92f8bb5ff25397676b1ea4561c15c1466a5c477
Ikechukwu Okoro <ikeshegs@gmail.com>
2018-10-23 16:37:25 +0100

    upload code

---
+++ src/robots.js
@@ -0,0 +1,68 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Rachael Francis',
+    username: 'Rachy',
+    email: 'rachy@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Chioma Okoro',
+    username: 'rochi',
+    email: 'rochi@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Chinomnso Okoro',
+    username: 'nonny',
+    email: 'nonny@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Ikechukwu Okoro',
+    username: 'ikeshegs',
+    email: 'ikeshegs@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Nkemakolam Nnadi',
+    username: 'kemason',
+    email: 'kemason@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Anasemi Osaro',
+    username: 'semi',
+    email: 'semi@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Ezinne Uwanta',
+    username: 'atappy',
+    email: 'atappy@gmail.com'
+  },
+  {
+    id: 8,
+    name: 'James Utaka',
+    username: 'meka',
+    email: 'meka@gmail.com'
+  },
+  {
+    id: 9,
+    name: 'Zabdiel Adams',
+    username: 'zabdy',
+    email: 'zabdy@gmail.com'
+  },
+  {
+    id: 10,
+    name: 'Mishel David',
+    username: 'david',
+    email: 'david@gmail.com'
+  },
+  {
+    id: 11,
+    name: 'Micah Anyang',
+    username: 'micah',
+    email: 'micah@gmail.com'
+  },
+];
